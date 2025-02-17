9bbb647e6b7592a087324b44956d179ceda131e7
Samed Zırhlıoğlu <zirhlioglusamed@gmail.com>
2025-02-17 04:43:47 +0300

    a

--- crg-template-5/script.js
+++ crg-template-5/script.js
@@ -3,6 +3,11 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
+            // Script Data
+            job_renewal_time: 2,
+            withdraw_amount: 0,
+            deposit_amount: 0,
+
             // User Data
             header: 'Haydar TAŞIYAN',
             level: 0,
@@ -11,9 +16,44 @@ createApp({
             total_earned: 0,
             completed_deliveries: 0,
             travelled_distance: 0,
+            has_convoy: true,
+            is_convoy_leader: true,
 
-            top_driver: [
+            convoy_requests: [
+                { name: 'japa brandao', exp: 76 },
+            ],
+
+            available_drivers: [
+                { name: 'japa brandao', exp: 76 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'john doe', exp: 50 }
+            ],
+
+            company_drivers: [
+                { name: 'japa brandao', exp: 76 },
+                { name: 'maria lopez', exp: 89 },
+                { name: 'john doe', exp: 50 }
+            ],
+
+            top_drivers: [
                 { name: 'japa brandao', distance: 2.69, exp: 76 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
+                { name: 'maria lopez', distance: 3.45, exp: 89 },
                 { name: 'maria lopez', distance: 3.45, exp: 89 },
                 { name: 'john doe', distance: 1.98, exp: 50 }
             ],
@@ -27,6 +67,12 @@ createApp({
                     desc: "This panel provides an overview of your company's progress, including earnings, deliveries, and top drivers' performance.",
                     icon: '<i class="fa-solid fa-house-user"></i>'
                 },
+                panel_convoy: {
+                    required_level: 0,
+                    name: 'Convoy',
+                    desc: "This panel provides you with the ability to create or join a convoy, drive together with other players, and complete jobs as a team.",
+                    icon: '<i class="fa-solid fa-users-rectangle"></i>'
+                },
                 panel_jobs_tier_1: {
                     required_level: 0,
                     name: 'Jobs Tier 1',
@@ -36,27 +82,51 @@ createApp({
                 panel_jobs_tier_2: {
                     required_level: 5,
                     name: 'Jobs Tier 2 (lv.5)',
-                    desc: '',
+                    desc: 'On this page, you can take Tier 2 trucking jobs, transport cargo, and earn experience. Start your journey now!',
                     icon: '<i class="fa-solid fa-radiation"></i>'
                 },
                 panel_jobs_tier_3: {
                     required_level: 10,
                     name: 'Jobs Tier 3 (lv.10)',
-                    desc: '',
+                    desc: 'On this page, you can take Tier 3 trucking jobs, transport cargo, and earn experience. Start your journey now!',
                     icon: '<i class="fa-solid fa-road-lock"></i>'
                 },
                 panel_jobs_tier_4: {
                     required_level: 10,
                     name: 'Jobs Tier 4 (lv.20)',
-                    desc: '',
-                    icon: '<i class="fa-solid fa-road-lock"></i>'
+                    desc: 'On this page, you can take Tier 4 trucking jobs, transport cargo, and earn experience. Start your journey now!',
+                    icon: '<i class="fa-solid fa-skull-crossbones"></i>'
                 },
                 panel_bank: {
                     required_level: 0,
                     name: 'Bank Account',
-                    desc: '',
+                    desc: 'On this page, you can manage your companies bank account.',
                     icon: '<i class="fa-solid fa-building-columns"></i>'
                 },
+            },
+
+            // Jobs
+            jobs_list: {
+                tier_1: [
+                    { reward: 11000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 11000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 11000, title: "Automobile Transport", distance: 8.5 },
+                ],
+                tier_2: [
+                    { reward: 12000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 12000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 12000, title: "Automobile Transport", distance: 8.5 },
+                ],
+                tier_3: [
+                    { reward: 13000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 13000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 13000, title: "Automobile Transport", distance: 8.5 },
+                ],
+                tier_4: [
+                    { reward: 14000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 14000, title: "Automobile Transport", distance: 8.5 },
+                    { reward: 14000, title: "Automobile Transport", distance: 8.5 },
+                ]
             }
         };
     },
@@ -66,12 +136,13 @@ createApp({
         set_active_panel(panel) {
             this.active_panel = panel;
         },
-        calculate_level_from_exp() {
+        calculate_level_from_exp(exp_to_calc = undefined) {
+            exp_to_calc = exp_to_calc ? exp_to_calc : this.exp
             let counter = 0;
             while (true) {
                 let exp_achieved = counter === 0 ? 0 : 500 + 250 * ((counter - 1) ** 2);
                 let exp_required = 500 + 250 * (counter ** 2);
-                if (exp_achieved <= this.exp && this.exp <= exp_required) {
+                if (exp_achieved <= exp_to_calc && exp_to_calc <= exp_required) {
                     return counter;
                 }
                 counter++;
@@ -92,6 +163,15 @@ createApp({
         },
         initialize_level() {
             this.level = this.calculate_level_from_exp();
+        },
+        fire_driver(driver_index) {
+            alert(driver_index)
+        },
+        withdraw_money() {
+            alert("withdraw_money")
+        },
+        deposit_money() {
+            alert("deposit_money")
         }
     },
     mounted() {
